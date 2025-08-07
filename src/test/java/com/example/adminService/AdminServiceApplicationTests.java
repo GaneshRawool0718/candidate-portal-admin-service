b00f994ca3c65c0d394c5c7a24ca6d345dff06c3
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-07 12:25:15 +0530

    chore: initial project setup for candidate-portal-admin-service

---
+++ src/test/java/com/example/adminService/AdminServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.adminService;
+
+// import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+	void contextLoads() {
+	}
+
+}
