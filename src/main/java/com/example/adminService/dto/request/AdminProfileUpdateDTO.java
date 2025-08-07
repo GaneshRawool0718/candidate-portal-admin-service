2759ca3b1d7afd9b091501c470bf8fe36d3424bb
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-08 01:19:52 +0530

    feat: add AdminProfileUpdateDTO for updating admin profile

---
+++ src/main/java/com/example/adminService/dto/request/AdminProfileUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.example.adminService.dto.request;
+
+import lombok.Data;
+
+@Data
+public class AdminProfileUpdateDTO {
+    // Fields for updating admin profile
+    private String name;
+}
