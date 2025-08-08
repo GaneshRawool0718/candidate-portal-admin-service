aa934f23647690b2cdecb8856f31fcf6934bb394
GaneshRawool0718 <ganesh.rawool@nonstopio.com>
2025-08-08 10:56:16 +0530

    feat: add require field for CandidateRequestDTO

---
+++ src/main/java/com/example/adminService/dto/request/CandidateRequestDTO.java
@@ -0,0 +1,12 @@
+package com.example.adminService.dto.request;
+
+import lombok.Data;
+
+@Data
+public class CandidateRequestDTO {
+    private String name;
+    private String email;
+    private String technology;
+    private String experience;
+}
+
